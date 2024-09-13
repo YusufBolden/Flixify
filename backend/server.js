6a06b0ef98d69836fad3c13958b994437fec9ce6
YusufBolden <yusuf@emergentworks.org>
2024-09-13 19:23:40 -0400

    Update file structure to create auth folders for routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,9 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 
-app.get("/signup", (req, res) => {
-  res.send("Signup route");
-});
-
-app.get("/login", (req, res) => {
-  res.send("Login route");
-});
-
-app.get("/logout", (req, res) => {
-  res.send("Logout route");
-});
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(5000, () => {
   console.log("Server started at http://localhost:5000");
